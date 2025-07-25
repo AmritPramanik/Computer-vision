b73c6c1b01bfb5e861819b4442e0e331fbd08192
Amrit Pramanik <pramanikamrit778@gmail.com>
2025-07-26 04:30:19 +0530

    Initial Commit

--- sample.py
+++ sample.py
@@ -1,7 +1,13 @@
 import cv2
 from ultralytics import YOLO
 
-model = YOLO('../Yolo-Weights/yolov8n.pt')
-results = model("../image/sample2.jpg",show = True)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+model = YOLO('../Yolo-Weights/yolov8l.pt')
+results = model("image/sample2.jpg")
+
+while True:
+    annotated_frame = results[0].plot()
+    cv2.imshow("YOLO V8", annotated_frame)
+    if cv2.waitKey(1) == ord('x'):
+        break
+
+cv2.destroyAllWindows()
